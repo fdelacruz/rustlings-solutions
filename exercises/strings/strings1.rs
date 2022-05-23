5946041e1dc789c6feeb92bd33d78bf6c6e1eacb
Francisco De La Cruz <helloquico@gmail.com>
2022-05-23 15:30:11 -0400

    strings1

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -1,8 +1,4 @@
 // strings1.rs
-// Make me compile without changing the function signature!
-// Execute `rustlings hint strings1` for hints ;)
-
-// I AM NOT DONE
 
 fn main() {
     let answer = current_favorite_color();
@@ -10,5 +6,5 @@ fn main() {
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    "blue".to_string()
 }
